6c33a1a6d68e0757a13442bf02bd6b78a60c6475
Isaac <iccolon818@gmail.com>
2021-10-08 13:41:12 -0400

    Initial branch commit- WASD/arrow movement + WAD resources

---
+++ lwjgl3/src/main/java/core/gdx/wad/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,24 @@
+package core.gdx.wad.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import core.gdx.wad.MyGDxTest;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+	public static void main(String[] args) {
+		createApplication();
+	}
+
+	private static Lwjgl3Application createApplication() {
+		return new Lwjgl3Application(new MyGDxTest(), getDefaultConfiguration());
+	}
+
+	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+		configuration.setTitle("GdxWadFuncs");
+		configuration.setWindowedMode(640, 480);
+		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+		return configuration;
+	}
+}
