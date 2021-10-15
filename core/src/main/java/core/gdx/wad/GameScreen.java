4ab42469eb0dd7040520ceb48d4041bb1d77c103
Isaac <iccolon818@gmail.com>
2021-10-15 14:34:36 -0400

    Player information is now taken from GameLogic, not stored in the Screen

--- core/src/main/java/core/gdx/wad/GameScreen.java
+++ core/src/main/java/core/gdx/wad/GameScreen.java
@@ -15,10 +15,6 @@ public class GameScreen implements Screen {
 
     Thread gameLogic;
 
-    //Player
-    PlayerPawn player;
-
-
     //screen
     OrthographicCamera camera;
     private final Vector2 mouseInWorld2D = new Vector2();
@@ -30,7 +26,6 @@ public class GameScreen implements Screen {
     public GameScreen(Thread gameLogic) {
         this.gameLogic = gameLogic;
         GameLogic.loadEntities(GameLogic.currentLevel);
-        this.player = GameLogic.getPlayer(0);
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1920, 1080);
         batch = new SpriteBatch();
@@ -49,17 +44,17 @@ public class GameScreen implements Screen {
         //This centers the camera to the player
         //Get the angle where the mouse is pointing to on the screen in relation to where the player is
         //Referenced code - https://stackoverflow.com/questions/16381031/get-cursor-position-in-libgdx
-        mouseInWorld3D.x = Gdx.input.getX() - player.getPos().x;
-        mouseInWorld3D.y = Gdx.input.getY() + player.getPos().y;
+        mouseInWorld3D.x = Gdx.input.getX() - GameLogic.getPlayer(0).getPos().x;
+        mouseInWorld3D.y = Gdx.input.getY() + GameLogic.getPlayer(0).getPos().y;
         mouseInWorld3D.z = 0;
         camera.unproject(mouseInWorld3D); //unprojecting will give game world coordinates matching the pointer's position
         mouseInWorld2D.x = mouseInWorld3D.x;
         mouseInWorld2D.y = mouseInWorld3D.y;
-        player.getPos().angle = mouseInWorld2D.angleDeg(); //Turn the vector2 into a degree angle
+        GameLogic.getPlayer(0).getPos().angle = mouseInWorld2D.angleDeg(); //Turn the vector2 into a degree angle
         //System.out.println(player.getPos().angle + ", " + player.getPos().x + ", " + player.getPos().y);
 
-        camera.position.set(player.getPos().x + player.getWidth()/(float)2.0,
-                player.getPos().y + player.getHeight()/(float)2.0, 0);
+        camera.position.set(GameLogic.getPlayer(0).getPos().x + GameLogic.getPlayer(0).getWidth()/(float)2.0,
+                GameLogic.getPlayer(0).getPos().y + GameLogic.getPlayer(0).getHeight()/(float)2.0, 0);
         camera.update();
 
         batch.setProjectionMatrix(camera.combined);
