3703ddf02775ec8eabaeb920b22eb4bc06b8da04
parth565 <47224076+parth565@users.noreply.github.com>
2021-10-20 10:14:39 -0400

    added a menu for co-op mode lobby

--- core/src/main/java/core/gdx/wad/MainMenuScreen.java
+++ core/src/main/java/core/gdx/wad/MainMenuScreen.java
@@ -9,23 +9,25 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.removeActor;
+
 public class MainMenuScreen implements Screen {
 
     public MyGDxTest game;
-    SpriteBatch batch;
-    OrthographicCamera camera;
     Stage stage;
-
     ImageButton play;
     ImageButton exit;
     Texture background;
-//    Texture exitButtonInactive
 
+    Skin uiSkin = new Skin(Gdx.files.internal("uiSkin.json"));
 
     public MainMenuScreen(MyGDxTest game) {
         this.game = game;
@@ -64,15 +66,34 @@ public class MainMenuScreen implements Screen {
         stage.draw(); //Draw the ui
 
 
-        play.addListener(new InputListener() {
+        play.addListener(new ClickListener() {
             @Override
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-//                titleScreen = new TitleScreen(this, gameLoop);
-//                setScreen(titleScreen);
-                }
-            @Override
-            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                return true;
+            public void clicked (InputEvent event, float x, float y) {
+                System.out.println("CLicked");
+                Window lobby = new Window("Lobby", uiSkin);
+                lobby.setMovable(false);
+                TextButton joinLobby = new TextButton("Join Lobby", uiSkin);
+                TextButton createLobby = new TextButton("Create Lobby", uiSkin);
+                TextButton back = new TextButton("Back", uiSkin);
+                lobby.add(createLobby).row();
+                lobby.add(joinLobby).row();
+                lobby.add(back);
+                lobby.setBounds((Gdx.graphics.getWidth() - 400)/ 2, (Gdx.graphics.getHeight() - 200) / 2, 400, 200);
+                stage.addActor(lobby);
+                back.addListener(new ClickListener(){
+                    public void clicked(InputEvent event, float x, float y) {
+                        game.setScreen(new MainMenuScreen(game));
+                    }
+                });
+                createLobby.addListener(new ClickListener(){
+                    public void clicked(InputEvent event, float x, float y) {
+
+                    }
+                });
+                joinLobby.addListener(new ClickListener(){
+                    public void clicked(InputEvent event, float x, float y) {
+                    }
+                });
             }
         });
 
