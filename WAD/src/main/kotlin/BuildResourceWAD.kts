5c05fb6c695c4fafa33bf4fd7aec80518500eaf9
Isaac <iccolon818@gmail.com>
2021-11-02 14:18:42 -0400

    Fixed script by using addData and sped up script to usable level by using newDirectoryStreams

--- WAD/src/main/kotlin/BuildResourceWAD.kts
+++ WAD/src/main/kotlin/BuildResourceWAD.kts
@@ -1,48 +1,65 @@
 import net.mtrop.doom.WadEntry
 import net.mtrop.doom.WadFile
 import java.io.File
+import java.nio.file.DirectoryStream
+import java.nio.file.Files
+import java.nio.file.Path
 
 val wad = WadFile.createWadFile(File("../../../../assets/resource.wad"))
+var stream: DirectoryStream<Path>
 
 val addFile = fun(file: File) {
     println(file.absolutePath)
-    wad.addData(file.name.substring(0, 8), file)
+    wad.addData(file.name.substring(0, 8.coerceAtMost(file.name.indexOf('.'))), file)
 }
 
 //Read graphics
 val graphics = File("../resources/graphics")
-graphics.listFiles().forEach { f -> addFile(f) }
+stream = Files.newDirectoryStream(graphics.toPath())
+stream.forEach {p -> addFile(p.toFile())}
+stream.close()
 
 //Read text lumps
 val textlmps = File("../resources/textlmps")
-textlmps.listFiles().forEach {f -> addFile(f)}
+stream = Files.newDirectoryStream(textlmps.toPath())
+stream.forEach {p -> addFile(p.toFile())}
+stream.close()
 
 //Read levels
 val levels = File("../resources/levels")
-levels.listFiles().forEach { f -> addFile(f) }
-
+stream = Files.newDirectoryStream(levels.toPath())
+stream.forEach {p -> addFile(p.toFile())}
+stream.close()
 //Read music
 var midis = File("../resources/midis")
 wad.addEntry(WadEntry.create("M_START"))
-midis.listFiles().forEach { f -> addFile(f) }
+stream = Files.newDirectoryStream(midis.toPath())
+stream.forEach {p -> addFile(p.toFile())}
+stream.close()
 wad.addEntry(WadEntry.create("M_END"))
 
 //Read sounds
 var sounds = File("../resources/sounds")
 wad.addEntry(WadEntry.create("FX_START"))
-sounds.listFiles().forEach { f -> addFile(f) }
+stream = Files.newDirectoryStream(sounds.toPath())
+stream.forEach {p -> addFile(p.toFile())}
+stream.close()
 wad.addEntry(WadEntry.create("FX_END"))
 
 //Read sprites
 var sprites = File("../resources/sprites")
 wad.addEntry(WadEntry.create("S_START"))
-sprites.listFiles().forEach { f -> addFile(f) }
+stream = Files.newDirectoryStream(sprites.toPath())
+stream.forEach {p -> addFile(p.toFile())}
+stream.close()
 wad.addEntry(WadEntry.create("S_END"))
 
 //Read tiles
 var tiles = File("../resources/tiles")
 wad.addEntry(WadEntry.create("G_START"))
-tiles.listFiles().forEach { f -> addFile(f) }
+stream = Files.newDirectoryStream(tiles.toPath())
+stream.forEach {p -> addFile(p.toFile())}
+stream.close()
 wad.addEntry(WadEntry.create("G_END"))
 
 wad.close()
