cc82a0c3c8ea9819e65534bcb01b4c0ea471d20a
Isaac <iccolon818@gmail.com>
2021-11-20 12:06:28 -0500

    App installs and opens, but can't find resource.wad

---
+++ android/src/main/java/core/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package core.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import core.gdx.wad.MyGDxTest;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+		initialize(new MyGDxTest(), configuration);
+	}
+}
