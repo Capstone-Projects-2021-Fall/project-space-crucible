521a0356043a4227c6ebb3e2fc8fdaa2b608232c
Isaac <iccolon818@gmail.com>
2021-12-06 13:24:48 -0500

    Remove methods after createApplication on main

--- lwjgl3/src/main/java/core/gdx/wad/lwjgl3/LegacyLauncher.java
+++ lwjgl3/src/main/java/core/gdx/wad/lwjgl3/LegacyLauncher.java
@@ -9,8 +9,6 @@ import core.wad.funcs.SoundFuncs;
 public class LegacyLauncher {
 	public static void main(String[] args) {
 		createApplication();
-		SoundFuncs.closeSequencer();
-		System.exit(0);
 	}
 
 	private static LwjglApplication createApplication() {
